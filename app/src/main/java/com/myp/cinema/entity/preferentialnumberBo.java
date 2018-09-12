ea60a5b28d0eb20d84b93752120c2460cf4dffa4
zhangchengku <13552008150@163.com>
2018-09-12 11:56:25 +0800

    9.12

---
+++ app/src/main/java/com/myp/cinema/entity/preferentialnumberBo.java
@@ -0,0 +1,19 @@
+package com.myp.cinema.entity;
+
+import java.io.Serializable;
+
+/**
+ * Created by Administrator on 2018/3/30.
+ */
+
+public class preferentialnumberBo implements Serializable {
+    private Integer globalCanBuyNum;
+
+    public Integer getGlobalCanBuyNum() {
+        return globalCanBuyNum;
+    }
+
+    public void setGlobalCanBuyNum(Integer globalCanBuyNum) {
+        this.globalCanBuyNum = globalCanBuyNum;
+    }
+}
